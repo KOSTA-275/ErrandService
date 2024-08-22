2650177eda058d17428527e42a60438e3f579662
LWH <lwhprt@naver.com>
2024-08-22 15:35:32 +0900

    프로젝트 생성

---
+++ src/main/java/com/dowadream/errand_service/ErrandServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dowadream.errand_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErrandServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErrandServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dowadream/errand_service/ErrandServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dowadream.errand_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErrandServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
