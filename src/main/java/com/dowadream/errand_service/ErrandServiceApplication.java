6a4ae050245f98b96e14e9d943a562a6e7f30b0e
LWH <lwhprt@naver.com>
2024-08-27 20:20:23 +0900

    유레카 의존성 추가 및 설정
    유레카 의존성 추가, 의존성 주입, config 작성하여 연결 테스트완료

--- src/main/java/com/dowadream/errand_service/ErrandServiceApplication.java
+++ src/main/java/com/dowadream/errand_service/ErrandServiceApplication.java
@@ -2,9 +2,11 @@ package com.dowadream.errand_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ErrandServiceApplication {
 
 	public static void main(String[] args) {
